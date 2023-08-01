f5d90a560e151de70b1001a124c23d04e06884be
ELIASHEAD <eliashead.costa@gmail.com>
2023-08-01 09:13:05 -0300

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      gridTemplateColumns: {
+        app: 'minmax(18rem, 20rem) 1fr',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
